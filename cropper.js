5a75e9d837f34b07bc7e2fc2fab036c3f85903c7
GFB <go.for.broke1006@gmail.com>
2016-07-26 11:16:20 +0300

    Add wildfowl file

--- cropper.js
+++ cropper.js
@@ -2,34 +2,6 @@
  * Created by scherk01 on 25.07.2016.
  */
 
-/**
- * Send post request with Ajax
- * @param {String} url
- * @param {FormData} formData
- * @param {Function} successCb
- */
-function ajaxPost(url, formData, successCb) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url, true);
-    xhr.onload = successCb;
-    xhr.send(formData);
-}
-
-function dataURItoBlob(dataURI) {
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-    return new Blob([ia], {type: mimeString});
-}
-
-
 var canvasEl = document.getElementById('canvas'),
     context2d = canvasEl.getContext('2d'),
     inputImageEl = document.getElementById('inputImage'),
