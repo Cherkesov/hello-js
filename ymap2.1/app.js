4d56c1a58b5fea62c7595ed6cd5bb70b923dcd07
GFB <go.for.broke1006@gmail.com>
2016-08-04 17:53:41 +0300

    Create demo for Yandex Maps 2.1 with clusterers

--- ymap2.1/app.js
+++ ymap2.1/app.js
@@ -18,64 +18,29 @@ function init() {
         iconImageOffset: [-15, -15]
     };
 
-    var placemarks = [
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 01', balloonContent: 'Столица России 01'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 02', balloonContent: 'Столица России 02'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 03', balloonContent: 'Столица России 03'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 04', balloonContent: 'Столица России 04'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 05', balloonContent: 'Столица России 05'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 06', balloonContent: 'Столица России 06'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 07', balloonContent: 'Столица России 07'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 08', balloonContent: 'Столица России 08'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 09', balloonContent: 'Столица России 09'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 10', balloonContent: 'Столица России 10'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 11', balloonContent: 'Столица России 11'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 12', balloonContent: 'Столица России 12'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 13', balloonContent: 'Столица России 13'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 14', balloonContent: 'Столица России 14'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 15', balloonContent: 'Столица России 15'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 16', balloonContent: 'Столица России 16'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 17', balloonContent: 'Столица России 17'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 18', balloonContent: 'Столица России 18'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 19', balloonContent: 'Столица России 19'}, options),
-        new ymaps.Placemark([55.76, 37.64], {hintContent: 'Москва! 20', balloonContent: 'Столица России 20'}, options),
+    var placemarks = [];
 
-
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 01', balloonContent: 'Столица России 01'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 02', balloonContent: 'Столица России 02'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 03', balloonContent: 'Столица России 03'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 04', balloonContent: 'Столица России 04'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 05', balloonContent: 'Столица России 05'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 06', balloonContent: 'Столица России 06'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 07', balloonContent: 'Столица России 07'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 08', balloonContent: 'Столица России 08'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 09', balloonContent: 'Столица России 09'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 10', balloonContent: 'Столица России 10'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 11', balloonContent: 'Столица России 11'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 12', balloonContent: 'Столица России 12'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 13', balloonContent: 'Столица России 13'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 14', balloonContent: 'Столица России 14'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 15', balloonContent: 'Столица России 15'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 16', balloonContent: 'Столица России 16'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 17', balloonContent: 'Столица России 17'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 18', balloonContent: 'Столица России 18'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 19', balloonContent: 'Столица России 19'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 20', balloonContent: 'Столица России 20'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 21', balloonContent: 'Столица России 21'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 22', balloonContent: 'Столица России 22'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 23', balloonContent: 'Столица России 23'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 24', balloonContent: 'Столица России 24'}, options),
-        new ymaps.Placemark([55.74, 37.62], {hintContent: 'Москва! 25', balloonContent: 'Столица России 25'}, options),
-
-
-        new ymaps.Placemark([55.78, 37.62], {hintContent: 'Москва! 01', balloonContent: 'Столица России 01'}, options),
-        new ymaps.Placemark([55.78, 37.62], {hintContent: 'Москва! 02', balloonContent: 'Столица России 02'}, options),
-        new ymaps.Placemark([55.78, 37.62], {hintContent: 'Москва! 03', balloonContent: 'Столица России 03'}, options),
-        new ymaps.Placemark([55.78, 37.62], {hintContent: 'Москва! 04', balloonContent: 'Столица России 04'}, options),
-        new ymaps.Placemark([55.78, 37.62], {hintContent: 'Москва! 05', balloonContent: 'Столица России 05'}, options),
-        new ymaps.Placemark([55.78, 37.62], {hintContent: 'Москва! 06', balloonContent: 'Столица России 06'}, options),
-        new ymaps.Placemark([55.78, 37.62], {hintContent: 'Москва! 07', balloonContent: 'Столица России 07'}, options),
-    ];
+    for (var i1 = 1; i1 <= 20; i1++) {
+        var mark1 = new ymaps.Placemark([55.76, 37.64], {
+            hintContent: 'Москва! ' + i1,
+            balloonContent: 'Столица России ' + i1
+        }, options);
+        placemarks.push(mark1);
+    }
+    for (var i2 = 1; i2 <= 25; i2++) {
+        var mark2 = new ymaps.Placemark([55.74, 37.62], {
+            hintContent: 'Москва! ' + i2,
+            balloonContent: 'Столица России ' + i2
+        }, options);
+        placemarks.push(mark2);
+    }
+    for (var i3 = 1; i3 <= 7; i3++) {
+        var mark3 = new ymaps.Placemark([55.78, 37.62], {
+            hintContent: 'Москва! ' + i3,
+            balloonContent: 'Столица России ' + i3
+        }, options);
+        placemarks.push(mark3);
+    }
 
     var clusterIcons = [
         {
@@ -99,7 +64,7 @@ function init() {
             }
         }];
     var myIconContentLayout = ymaps.templateLayoutFactory.createClass(
-        '<div style="color: #FFFFFF; font-weight: bold;">{{ properties.geoObjects.length }}</div>')
+        '<div style="color: #FFFFFF; font-weight: bold;">{{ properties.geoObjects.length }}</div>');
     var clusterer = new ymaps.Clusterer({
         clusterDisableClickZoom: true,
 
